07c02e8da62bf51a1c31ee55486db977cc779b51
Seth Corker <seth.corker@gmail.com>
2018-05-02 06:48:09 +1200

    Formatting

--- src/Category.js
+++ src/Category.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Product from './Product';
+import React from "react";
+import Product from "./Product";
 
 export default ({ title, products }) => (
-    <div>
-        <h2>{title}</h2>
-        <section className="category-section">
-            {products && products.map(Product)}
-        </section>
-    </div>
+  <div>
+    <h2>{title}</h2>
+    <section className="category-section">
+      {products && products.map(Product)}
+    </section>
+  </div>
 );
